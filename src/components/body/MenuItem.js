802388b79953b535fa042f3eca596b29d40d5254
Sudipta Saha Niloy <niloy305968@gmail.com>
2021-07-08 18:17:54 +0600

    Designing Menu Item

--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -1,8 +1,24 @@
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardBody, CardTitle } from 'reactstrap';
+import './Menuitem.css';
+
 const MenuItem = (props) => {
     console.log(props);
     return (
         <div>
-            
+            <Card className="Card">
+                <CardBody>
+                    <CardImg
+                        className="CardImg"
+                        width="100%"
+                        alt={props.dish.name}
+                        src={props.dish.image}
+                    />
+                    <CardImgOverlay>
+                        <CardTitle>{props.dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </CardBody>
+            </Card>
         </div>
     )
 }
